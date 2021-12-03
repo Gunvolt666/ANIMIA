f61e709d94b5f493a7bbad71c29d44e65845d582
Gunvolt666 <40903940+Gunvolt666@users.noreply.github.com>
2021-12-03 01:55:11 -0600

    cambios

--- xor_test/xor.js
+++ xor_test/xor.js
@@ -1,11 +1,11 @@
-// provide optional config object (or undefined). Defaults shown.
+
 const config = {
-    binaryThresh: 0.5, // ¯\_(ツ)_/¯
-    hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
-    activation: 'sigmoid' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
+    binaryThresh: 0.5,
+    hiddenLayers: [3],
+    activation: 'sigmoid' 
   };
   
-  // create a simple feed forward neural network with backpropagation
+
   const net = new brain.NeuralNetwork(config);
   
   net.train([{
